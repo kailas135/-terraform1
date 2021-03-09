01d35ca340ccaf0f3259279541e15384f59f4b84
demo1 <demo1@gmail.com>
2021-03-09 08:36:25 +0530

    minior change done in the script

---
+++ ab.tf
@@ -0,0 +1,21 @@
+terraform {
+    required_providers {
+        aws={
+            source="hashicorp/aws"
+        }      
+    }
+}
+
+
+provider "aws" {
+    profile = "default"
+    region = "us-east-1"
+  
+}
+
+
+resource "aws_instance" "kailas_instance" {
+    ami = "ami-0915bcb5fa77e4892"
+    instance_type = "t2.microterra"
+  
+}
