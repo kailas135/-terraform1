457ad7f964037716a3256b642c90cc95c2004d89
demo1 <demo1@gmail.com>
2021-03-09 10:02:24 +0530

    NEW code addded for terra:

--- ab.tf
+++ ab.tf
@@ -14,8 +14,8 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "kailas_instance" {
+resource "aws_instance" "kailas_instance_1" {
     ami = "ami-0915bcb5fa77e4892"
-    instance_type = "t2.microterra"
+    instance_type = "t2.micro"
   
 }
